c0d069c9896a39b2de2bfcbd2a6da178ba1bca9e
juanestebancarvaja <160348332+juanestebancarvaja@users.noreply.github.com>
2024-02-17 14:22:16 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(150,200,180);
+  circle(200,200,300)
+  circle(140,140,40)
+  circle(255,140,40)
+  rect(175,250,50,10)
+}
